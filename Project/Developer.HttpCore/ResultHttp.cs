a5962be11b1d8a0f31086ed22ef626a7368c3626
Rodrigo de Almeida Francisco <rodrigoalmeidafrancisco@gmail.com>
2023-04-07 22:21:44 -0300

    Subindo os arquivos em um novo repositório e atualizado o objeto de retorno (ResultHttp) para exibir mensagem de erro ao realizar a conversão do obejto de retorno.

---
+++ Project/Developer.HttpCore/ResultHttp.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Net;
+
+namespace Developer.HttpCore
+{
+    public class ResultHttp
+    {
+        public ResultHttp()
+        {
+            Success = true;
+            DataString = null;
+            DataBytes = null;
+        }
+
+        public bool Success { get; set; }
+
+        public string Message { get; set; }
+
+        public HttpResponseMessage Response { get; set; }
+
+        public HttpStatusCode HttpStatusCode { get; set; }
+
+        public string DataString { get; set; }
+
+        public byte[] DataBytes { get; set; }
+
+        public Stream DataStream { get; set; }
+
+        public bool RequestRejected { get; set; }
+    }
+}
